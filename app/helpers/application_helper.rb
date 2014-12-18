d65efa20df290d54c48d20b104b4a2d3d4c68a3f
Ahmed AlSum <aalsum@stanford.edu>
2014-12-18 12:13:26 -0800

    Add collection id helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def format_druid druid
+    if druid.nil? or druid.blank? then
+      return druid
+    else
+      return "#{Rails.configuration.argo_catalog}#{druid}"
+    end 
+  end
+  
+     # @collections_list = [["aaa","value"],["bbb","bbb"]]
+
+  def format_collection(collection_id, collections_list)
+    
+    if collection_id.nil? or collection_id.blank? then
+      return collection_id
+    end
+    
+    collections_list.each do |collection_record|
+      if collection_id == collection_record[1] then
+        return "<a href='"+format_druid(collection_id)+"' >"+collection_record[0]+"</a>"
+      end 
+    end
+  end
 end
