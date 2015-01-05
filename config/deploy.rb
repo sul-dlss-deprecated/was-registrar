f4a8bf5fd75c9f5f59f1aa852d17569f3c83cfb9
Ahmed AlSum <aalsum@stanford.edu>
2015-01-05 11:45:49 -0800

    Update deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, '/home/lyberadmin/was-registrar'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache config/environments tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache config/environments tmp/sockets vendor/bundle public/system db}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -58,3 +58,6 @@ namespace :deploy do
   end
 
 end
+
+after "deploy", "deploy:migrate"
+
