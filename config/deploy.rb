4d0a8f287e4dfab2481832f8bd5b834f38bd38f2
Ahmed AlSum <aalsum@stanford.edu>
2015-02-10 15:20:13 -0800

    Update capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/opt/app/was/was-registrar'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log config/environments  vendor/bundle public/system }
