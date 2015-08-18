3ef6cec61bef838e6b15037bddb1e033d23f5958
Ahmed AlSum <aalsum@stanford.edu>
2015-08-17 17:34:16 -0700

    Adding rake task to register crawl object from file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,15 @@ require_relative '../lib/was/importer/data_importer'
 require_relative '../lib/was/importer/data_importer_results'
 require_relative '../lib/was/importer/data_importer_from_text'
 require_relative '../lib/was/importer/data_importer_verifier'
+
 require_relative '../lib/was/registrar/register_seed_object'
 require_relative '../lib/was/registrar/register_crawl_object'
+require_relative '../lib/was/registrar/register_crawl_object_from_file'
 require_relative '../lib/was/registrar/source_xml_writer'
 require_relative '../lib/was/registrar/sync_crawl_object'
+
 require_relative '../lib/was/utilities/dor_utilities'
+
 require 'rspec/matchers'
 require 'equivalent-xml'
 require 'nokogiri'
