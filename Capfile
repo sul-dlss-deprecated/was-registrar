c70535d73f0a475cfb8d5c76a7731327c2cd74ae
Naomi Dushay <ndushay@stanford.edu>
2016-06-22 18:08:32 -0700

    update deploy scripts further per cbeer PR

--- Capfile
+++ Capfile
@@ -3,7 +3,6 @@ require 'capistrano/setup'
 
 # Includes default deployment tasks
 require 'capistrano/deploy'
-require 'capistrano/one_time_key'
 
 # Includes tasks from other gems included in your Gemfile
 #
@@ -19,6 +18,7 @@ require 'capistrano/rvm'
 # require 'capistrano/rbenv'
 # require 'capistrano/chruby'
 require 'capistrano/bundler'
+require 'capistrano/passenger'
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-set :rvm_ruby_string, 'ruby-2.1.4'
-
 set :application, 'was-registrar'
 set :repo_url, 'https://github.com/sul-dlss/was-registrar.git'
 
@@ -34,32 +32,3 @@ set :linked_dirs, %w{bin log config/environments  vendor/bundle public/system da
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-namespace :deploy do
-
-  task :start do
-    bundle exec cap setup
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
-
-after "deploy", "deploy:migrate"
