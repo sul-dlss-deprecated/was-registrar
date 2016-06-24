89147d581f54c6a9d15909389b4323b1b7686835
Darren Hardy <drh@stanford.edu>
2016-06-24 15:24:29 -0700

    explicitly set the RAILS_ENV to production for stage

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -4,3 +4,4 @@ Capistrano::OneTimeKey.generate_one_time_key!
 
 set :bundle_without, %w{deployment development test}.join(' ')
 set :deploy_environment, 'production'
+set :rails_env, fetch(:deploy_environment)
