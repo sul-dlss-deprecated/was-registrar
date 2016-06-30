d072c4ddd1e5b23b3dd494bfdfc0ae5a62070b72
Darren Hardy <drh@stanford.edu>
2016-06-30 14:38:47 -0700

    remove the Manage crawls tab and UI (see github issue #29 and #30)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
- 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'seeds#index'
 
-  get 'crawls/index' => 'crawls#index'
   match ':controller(/:action(/:id))', :via=>  [:get, :post]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- lib/was/registrar/sync_crawl_object.rb
+++ lib/was/registrar/sync_crawl_object.rb
@@ -1,50 +1,55 @@
 module Was
   module Registrar
-    
+
     # makes syncronization between the available crawl jobs, database, and DOR repo.
-    class SyncCrawlObject 
+    class SyncCrawlObject
 
       def sync_all
         sync_staging
         sync_dor
       end
-      
+
       # Reads the job directories from the staging mount
       def sync_staging
-        
+
         get_jobs_directories.each do | job_dir |
           crawl_item = CrawlItem.find_by job_directory: job_dir
-          
+
           if crawl_item.nil?
             CrawlItem.create({:job_directory=>job_dir, :on_disk=> true })
           else
             crawl_item.update(:on_disk=>true)
-          end 
+          end
         end
       end
-      
+
       def get_jobs_directories
         jobs_directory = "#{Rails.configuration.crawl_stage}"
         absoulte_job_dir_list = Dir.glob("#{jobs_directory}*/2*/") # {|f| File.directory? f}
-        
+
         short_job_dir_list = []
         absoulte_job_dir_list.each do | absoulte_job_dir |
           short_job_dir_list.append( absoulte_job_dir.sub(jobs_directory,"")[0..-2] )
         end
-        
+
         return short_job_dir_list
       end
-      
+
       # Reads the crawl objects from DOR
+      #
+      # TODO: We removed the implementation of this code since it was using
+      # the REST interface to `dor-fetcher`, which is being phased out, to
+      # fetch all items associated with the crawl APOs
       def sync_dor
-        items_list = Was::Utilities::DorUtilities.new(Rails.configuration.crawl_apos).get_items_list
+        items_list = []
+        fail NotImplementedError, 'Need to fetch all items associated with all the Crawl APOs'
 
         items_list.each do |item|
           crawl_item = CrawlItem.find_by job_directory: item[:title]
           unless crawl_item.nil?
             crawl_item.update(:druid_id => item[:druid])
           end
-        end    
+        end
       end
 
     end
