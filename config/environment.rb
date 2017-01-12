c8eb8d5739c6dccb3c4704efa0b91e2901b2b0a0
Naomi Dushay <ndushay@stanford.edu>
2017-01-12 00:20:38 -0800

    config/environment.rb: remove silly comment

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-#Rails.logger = Logger.new(File.join(File.dirname(__FILE__), "..", "log", "was-registrar.log"))
