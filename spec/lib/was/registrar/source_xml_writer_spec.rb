19ce18e2b283973f4219d1b1e55b6d313cabc5e7
aaron-collier <aaron.collier@stanford.edu>
2019-01-08 15:14:26 -0800

    Fix hash alignment

--- spec/lib/was/registrar/source_xml_writer_spec.rb
+++ spec/lib/was/registrar/source_xml_writer_spec.rb
@@ -12,9 +12,13 @@ describe Was::Registrar::RegisterSeedObject do
   describe '.write_xml' do
     it 'should write a valid xml file' do
       registrar = Was::Registrar::SourceXmlWriter.new('./')
-      seed_hash = {'id'=>23, 'druid_id'=>'druid:aa111aa1111', 'uri'=>'http://www.slac.stanford.edu/',
-                    'embargo'=>false, 'source'=>nil, 'collection_id'=>'druid:gz033bg3146',
-                    'source_xml'=>"<seed>\n  <url>http://www.slac.stanford.edu/</url>\n  <active>true</active>\n  <public>true</public>\n
+      seed_hash = {'id' => 23,
+                   'druid_id' => 'druid:aa111aa1111',
+                   'uri' => 'http://www.slac.stanford.edu/',
+                   'embargo' => false,
+                   'source' => nil,
+                   'collection_id' => 'druid:gz033bg3146',
+                   'source_xml' => "<seed>\n  <url>http://www.slac.stanford.edu/</url>\n  <active>true</active>\n  <public>true</public>\n
                       <createdDate>2013-01-27 21:34:12 UTC</createdDate>\n  <lastUpdatedDate>2014-09-24 19:37:17 UTC</lastUpdatedDate>\n
                      <metadata>\n    <title>United States Global Change Research Program</title>\n    <creator>U.S. Global Change Research
                      Program</creator>\n    <publisher>U.S. Global Change Research Program</publisher>\n    <collector>Stanford University,
@@ -23,8 +27,13 @@ describe Was::Registrar::RegisterSeedObject do
                        <description>Web site of the United States Global Change Research Program. The USGCRP coordinates and integrates federal
                      research on climate change and changes in the global environment, as well as their implication for society.</description>\n
                        <language>eng</language>\n  </metadata>\n</seed>",
-                    'source_file'=>nil, 'import_date'=>nil, 'created_at'=>'Tue, 09 Dec 2014 18:55:54 UTC +00:00',
-                    'updated_at'=>'Thu, 11 Dec 2014 15:47:47 UTC +00:00', 'verified'=>nil, 'source_id'=>'was:a', 'rights'=>'world'}
+                   'source_file' => nil,
+                   'import_date' => nil,
+                   'created_at' => 'Tue, 09 Dec 2014 18:55:54 UTC +00:00',
+                   'updated_at' => 'Thu, 11 Dec 2014 15:47:47 UTC +00:00',
+                   'verified' => nil,
+                   'source_id' => 'was:a',
+                   'rights' => 'world'}
       registrar.write_xml( seed_hash )
 
       actual_xml_file_path = './druid:aa111aa1111.xml'
